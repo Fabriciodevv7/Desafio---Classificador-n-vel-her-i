faf6ea1733211b8348baf0c7b172ba06fe9d0e4e
Fabricio <fabriciodevv@gmail.com>
2025-07-28 13:32:19 -0300

    Desafio Heroi

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 const prompt = require("prompt-sync")();
 
 let nomeHeroi = prompt("Digite o nome do herói: ");
-let nivelHeroi = prompt("Digite o nível do herói: ");
+let nivelHeroi = parseInt(prompt("Digite o nível do herói: "));
 
 if (nivelHeroi < 1000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Ferro");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Ferro");
 }
 
 else if (nivelHeroi < 2000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Bronze");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Bronze");
 }
 
 else if (nivelHeroi < 5000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Prata");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Prata");
 }
 
 else if (nivelHeroi < 7000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Ouro");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Ouro");
 }
 
 else if (nivelHeroi < 8000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Platina");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Platina");
 }
 
 else if (nivelHeroi < 9000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Ascendente");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Ascendente");
 }
 
 else if (nivelHeroi < 10000) {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Imortal");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Imortal");
 }
 
 else {
-    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Radiante");
+    console.log("O herói de nome " + nomeHeroi + " está no nível: Radiante");
 }
