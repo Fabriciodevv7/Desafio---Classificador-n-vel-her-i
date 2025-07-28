c2acf3f90e4b286d85fb7a526149ab8663585de7
Fabricio <fabriciodevv@gmail.com>
2025-07-28 13:21:27 -0300

    Classificador de nível

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-let fabricio = "Fabricio";
+const prompt = require("prompt-sync")();
 
-console.log("Olá mundo");
+let nomeHeroi = prompt("Digite o nome do herói: ");
+let nivelHeroi = prompt("Digite o nível do herói: ");
 
+if (nivelHeroi < 1000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Ferro");
+}
+
+else if (nivelHeroi < 2000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Bronze");
+}
+
+else if (nivelHeroi < 5000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Prata");
+}
+
+else if (nivelHeroi < 7000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Ouro");
+}
+
+else if (nivelHeroi < 8000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Platina");
+}
+
+else if (nivelHeroi < 9000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Ascendente");
+}
+
+else if (nivelHeroi < 10000) {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Imortal");
+}
+
+else {
+    console.log("Olá herói: " + nomeHeroi + " O seu nível é: Radiante");
+}
